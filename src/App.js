5a7bab246414f53b75238e301429f4a2a9d306f2
Adithya <adithyamaheshbariki@gmail.com>
2019-10-08 20:03:18 -0400

    Working on the Nav bar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useSpring, animated } from "react-spring";
 import { Toggle } from "./Toggle";
+import Nav from "./Nav";
 import logo from "./logo.svg";
 import "./App.css";
 
---
+++ src/Nav.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Nav = () => {
+  return (
+    <div className="nav-wrapper">
+      <nav>
+        <a href="#">Home</a>
+        <a href="#">About</a>
+        <a href="#">Store</a>
+        <a href="#">Tutorials</a>
+      </nav>
+    </div>
+  );
+};
+
+export default Nav;
